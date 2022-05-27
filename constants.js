d14eb7a3a687e39d33c94cc125e2b61516acacba
ryansyn <90420703+ryansyn@users.noreply.github.com>
2022-05-26 19:24:13 -0500

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,9 @@
+const CLAN_ROLES = {
+    "leader": "Leader",
+    "coLeader": "Co-leader",
+    "admin": "Elder",
+    "member": "Member",
+    "not_member": "Clanless"
+};
+
+module.exports = { CLAN_ROLES };
